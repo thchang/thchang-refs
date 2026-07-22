db89ac6e58a1c65ec646c85107d0be3a780fcdd3
thchang <tyler@malayy.com>
2026-07-22 13:33:20 -0700

    update summaries

--- update_central.sh
+++ update_central.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-bibmgr clear --force
+python3 -m bibmgr clear --force
 for file in old_bibs/*.bib; do
-    bibmgr load -f $file -m --force
+    python3 -m bibmgr load -f $file -m --force
 done
-bibmgr load -f bibmgr_db.yaml -m --force
-rm bibmgr_db.yaml
-bibmgr save --force
+python3 -m bibmgr load -f bibmgr_db.yaml -m --force
+rm -f bibmgr_db.yaml
+python3 -m bibmgr save --force
 
 cat workflow/readme_head > README.md
 python3 workflow/make_readme.py AI SciML optimization HPC software \
